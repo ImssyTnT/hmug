d3b6354fd0d25222408b2259f0921053b3d7b63d
ImssyTnT <562285429@qq.com>
2022-09-07 08:59:39 +0800

    完成了home页面

---
+++ api/home.js
@@ -0,0 +1,10 @@
+import fly from '@/utils/request.js'
+
+// 获取首页轮播图
+export const getBanners = () => fly.get('/home/swiperdata')
+
+// 获取首页导航
+export const getNavList = () => fly.get('/home/catitems')
+
+// 获取楼层
+export const getFloors = () => fly.get('/home/floordata')
---
+++ utils/toast.js
@@ -0,0 +1,28 @@
+// 普通提示
+const toast = (title, duration = 3000) => {
+  uni.showToast({
+    title,
+    icon: 'none',
+    duration
+  })
+}
+
+// 错误提示
+toast.err = (title, duration = 3000) => {
+  uni.showToast({
+    title,
+    icon: 'error',
+    duration
+  })
+}
+
+// 成功提示
+toast.succes = (title, duration = 3000) => {
+  uni.showToast({
+    title,
+    icon: 'succes',
+    duration
+  })
+}
+
+export default toast
